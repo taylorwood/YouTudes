7f70a4517cf5ec1f5f44e13c69ec0115ea8b7018
Taylor Wood <taylorwood@me.com>
2015-12-17 07:24:31 -0600

    Add more opportunities for silent failure.

---
+++ Utility.fs
@@ -0,0 +1,16 @@
+﻿namespace YouTudes
+
+module Utility =
+
+    // http://fsharpforfunandprofit.com/posts/computation-expressions-intro/
+    type MaybeBuilder() =
+
+        member this.Bind(x, f) = 
+            match x with
+            | None -> None
+            | Some a -> f a
+
+        member this.Return(x) = 
+            Some x
+   
+    let maybe = new MaybeBuilder()
--- YouTubeComments.fs
+++ YouTubeComments.fs
@@ -3,14 +3,18 @@
 open System
 open System.Text.RegularExpressions
 open FSharp.Data
+open Utility
 
 module YouTubeComments =
 
-    /// Gets a Match's Group value if it succeeded, otherwise an empty string
+    /// Gets a Match's Group value if it succeeded, otherwise None
     let getRegexGroupValue (regex: Regex) (group: string) input =
         match regex.Match input with
-        | m when m.Success -> m.Groups.[group].Value
-        | _ -> String.Empty
+        | m when m.Success ->
+            let value = m.Groups.[group].Value
+            if String.IsNullOrWhiteSpace value then None
+            else Some value
+        | _ -> None
 
     let videoIdRegex = Regex(@"/watch\?v=(?<Id>\w+)", RegexOptions.Compiled)
     let getVideoId = getRegexGroupValue videoIdRegex "Id"
@@ -34,6 +38,7 @@ module YouTubeComments =
             |> Seq.collect (fun n -> n.Elements("a"))
             |> Seq.choose (fun n -> n.TryGetAttribute("href"))
             |> Seq.map (fun attr -> host + attr.Value())
+            |> Seq.distinct
 
         let cookies = System.Net.CookieContainer() // required for comment JSON requests
 
@@ -52,15 +57,25 @@ module YouTubeComments =
         let getVideoCommentJson videoUrl =
             printfn "Requesting video page for %s..." videoUrl
             let videoHtml = Http.RequestString(videoUrl, cookieContainer = cookies)
-            let token = getSessionToken videoHtml
-            getVideoId videoUrl |> getCommentUrl |> getCommentJson token videoUrl
+            maybe {
+                let! token = getSessionToken videoHtml
+                let! videoId = getVideoId videoUrl
+                return getCommentUrl videoId |> getCommentJson token videoUrl
+            }
 
         // request and parse the JSON comments
         let commentBodies =
             videoUrls
-            |> Seq.map getVideoCommentJson
+            |> Seq.choose getVideoCommentJson
             |> Seq.map JsonValue.Parse
-            |> Seq.map (fun j -> j.GetProperty("body").GetProperty("watch-discussion"))
+            |> Seq.map
+                (fun j ->
+                    maybe {
+                        let! body = j.TryGetProperty("body")
+                        let! html = body.TryGetProperty("watch-discussion")
+                        return html
+                    })
+            |> Seq.choose id
         
         // parses the embedded HTML from a JSON comment result, returning the comment text
         let getCommentText (body: JsonValue) =
